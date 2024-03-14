82baa096ed8789f982421f31a32b3dbdaf482fee
DOF1109 <friasdaniel.1109@gmail.com>
2024-03-14 14:20:09 -0300

    Se organiza el proyecto y se instala React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./components/pages/Home";
 
 function App() {
   return (
-   <div>
-      <h1>Mas que solo bebidas, festejemos el encuentro.</h1> 
-   </div>
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+   </BrowserRouter>
   )
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Card from "./components/Card"
+import Card from "../common/Card"
 
 const Home = () => {
   const [beers, setBeers] = useState([])
@@ -16,6 +16,7 @@ const Home = () => {
 
   return (
     <div className='grid'>
+      <h1>Home</h1>
         {beers.length 
            ? beers.map(beer => (<Card data={beer} />))
            : null
